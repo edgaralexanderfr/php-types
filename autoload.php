fed0662834651afee14f769f6f82aac8c3441379
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2024-07-28 12:29:35 -0500

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+function php_types_autoload(string $class): void
+{
+    $project_root_namespace = 'PHPTypes\\';
+
+    if (!str_contains($class, $project_root_namespace)) {
+        return;
+    }
+
+    $class_path = str_replace([$project_root_namespace, '\\'], ['', '/'], $class);
+    $file_path = __DIR__ . "/src/{$class_path}.php";
+
+    if (file_exists($file_path)) {
+        require_once $file_path;
+    }
+}
+
+spl_autoload_register('php_types_autoload');
+
+include __DIR__ . '/src/Primitive/types.php';
