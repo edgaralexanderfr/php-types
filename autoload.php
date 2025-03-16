705ff670540ffcf8620657ebddffa22dcdeb3eb5
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-03-16 15:20:37 -0500

    Add include guard to autoload.php

--- autoload.php
+++ autoload.php
@@ -2,28 +2,32 @@
 
 declare(strict_types=1);
 
-function php_types_autoload(string $class): void
-{
-    $project_root_namespace = 'PHPTypes\\';
+if (!defined('PHPTYPES')) {
+    define('PHPTYPES', 1);
 
-    if (!str_contains($class, $project_root_namespace)) {
-        return;
+    function php_types_autoload(string $class): void
+    {
+        $project_root_namespace = 'PHPTypes\\';
+
+        if (!str_contains($class, $project_root_namespace)) {
+            return;
+        }
+
+        $class_path = str_replace([$project_root_namespace, '\\'], ['', '/'], $class);
+        $file_path = __DIR__ . "/src/{$class_path}.php";
+
+        if (file_exists($file_path)) {
+            require_once $file_path;
+        }
     }
 
-    $class_path = str_replace([$project_root_namespace, '\\'], ['', '/'], $class);
-    $file_path = __DIR__ . "/src/{$class_path}.php";
+    spl_autoload_register('php_types_autoload');
 
-    if (file_exists($file_path)) {
-        require_once $file_path;
-    }
+    include __DIR__ . '/src/Data/types.php';
+    include __DIR__ . '/src/Std/types.php';
+    include __DIR__ . '/src/Primitive/types.php';
+    include __DIR__ . '/src/Returnable/types.php';
+    include __DIR__ . '/src/typedef.php';
+    include __DIR__ . '/src/typeof.php';
+    include __DIR__ . '/src/phppp.php';
 }
-
-spl_autoload_register('php_types_autoload');
-
-include __DIR__ . '/src/Data/types.php';
-include __DIR__ . '/src/Std/types.php';
-include __DIR__ . '/src/Primitive/types.php';
-include __DIR__ . '/src/Returnable/types.php';
-include __DIR__ . '/src/typedef.php';
-include __DIR__ . '/src/typeof.php';
-include __DIR__ . '/src/phppp.php';
