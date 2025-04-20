1cd5508397a36e9acc022c295677750e8cd0c60f
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-04-19 19:37:20 -0500

    Fix definition of `$working_directory` at phppp.php

--- src/phppp.php
+++ src/phppp.php
@@ -4,22 +4,36 @@ declare(strict_types=1);
 
 // Check and `define` `global` configuration from repositories:
 if (!defined('PHPPP_CONFIG')) {
-    $working_directory = getcwd();
+    global $argv, $_SERVER;
+
+    $working_directory = null;
+
+    if (isset($argv[0]) && ($working_directory = realpath($argv[0])) !== false) {
+        $working_directory = dirname($working_directory);
+    } else if (
+        ($working_directory = getcwd()) === false
+        && isset($_SERVER['SCRIPT_NAME']) && isset($_SERVER['SCRIPT_FILENAME'])
+    ) {
+        $working_directory = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['SCRIPT_FILENAME']);
+    }
+
     $directory = $working_directory;
     $config_file_found = false;
 
-    /** @todo We gotta handle Windows case in here... */
-    do {
-        $config_file_path = "{$directory}/phppp.json";
+    if ($directory) {
+        /** @todo We gotta handle Windows case in here... */
+        do {
+            $config_file_path = $directory . DIRECTORY_SEPARATOR . 'phppp.json';
 
-        if (file_exists($config_file_path)) {
-            $config_file_found = true;
+            if (file_exists($config_file_path)) {
+                $config_file_found = true;
 
-            break;
-        }
+                break;
+            }
 
-        $directory = realpath("{$directory}/../");
-    } while ($directory != '/');
+            $directory = realpath($directory . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+        } while ($directory != DIRECTORY_SEPARATOR);
+    }
 
     if (!$config_file_found || !is_object($config = json_decode((string) file_get_contents($config_file_path)))) {
         $config = (object)[];
